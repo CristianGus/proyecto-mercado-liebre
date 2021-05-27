5f8a839faaff81cf7560bdd91fb216811dd2ee4a
CristianGus <cristian.leivalot@gmail.com>
2021-05-27 20:24:18 -0300

    mercado liebre

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const publicPath = path.resolve(__dirname, './public')
+app.use(express.static(publicPath))
+
+
+app.set("port", process.env.PORT || 3000)
+
+app.listen(app.get("port"), () => {
+    console.log('Serividor corriendo en el puerto 3000'+app.get("port"));})
+
+app.use(express.static(path.resolve(__dirname, "./public")))
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html' )));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html' )));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html' )));
