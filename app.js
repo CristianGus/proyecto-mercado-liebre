6dd5788c6c42f99084e7d8dfdefeffb5a52186ca
CristianGus <cristian.leivalot@gmail.com>
2021-05-27 20:49:52 -0300

    deploy

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ app.use(express.static(publicPath))
 
 app.set("port", process.env.PORT || 3000)
 
-app.listen(app.get("port"), () => {
-    console.log('Serividor corriendo en el puerto 3000'+app.get("port"));})
+app.listen(app.get("port"), () =>
+    console.log('Servidor corriendo en el puerto 3000'+app.get("port")));
 
 app.use(express.static(path.resolve(__dirname, "./public")))
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html' )));
-app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html' )));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html' )));git 
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html' )));
